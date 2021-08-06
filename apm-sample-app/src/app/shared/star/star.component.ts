7ea807b909e4aa2128d3dc935643b89d251ac4a9
André Davis <CubicleJockey@users.noreply.github.com>
2021-08-05 17:04:06 -0700

    Input directive

--- apm-sample-app/src/app/shared/star/star.component.ts
+++ apm-sample-app/src/app/shared/star/star.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
      selector: 'pm-star'
     ,templateUrl: './star.component.html'
     ,styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnChanges{
+export class StarComponent implements OnChanges {
     public cropWidth: number = 75;
-    public rating: number = 4;
+
+    @Input()
+    public rating: number = 0;
 
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;
