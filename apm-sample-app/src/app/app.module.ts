6185cbd299231ac173585175517402f6d00ffe98
André Davis <CubicleJockey@users.noreply.github.com>
2021-08-05 17:58:22 -0700

    Created a ProductsService

--- apm-sample-app/src/app/app.module.ts
+++ apm-sample-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces-pipe';
 import { StarComponent } from './shared/star/star.component';
+import { ProductService } from './products/product.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { StarComponent } from './shared/star/star.component';
      BrowserModule //Needed so that app runs correctly within a browser.
     ,FormsModule 
   ],
-  providers: [],
+  providers: [ ProductService ],
   bootstrap: [ AppComponent ] //Starting component for application start-up
 })
 export class AppModule { }
--- apm-sample-app/src/app/products/product-list.component.ts
+++ apm-sample-app/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -13,7 +14,12 @@ export class ProductListComponent implements OnInit {
     public imageMargin: number = 2;
     public showImage: boolean = false;
     public message: string = '';
+
     
+    constructor(private productService: ProductService){
+        this.products = this.productService.getProducts();
+    }
+
     ngOnInit(): void {
         console.log('OnInit life-cycle hook has fired.');
         this.filter = 'cart';
@@ -39,60 +45,10 @@ export class ProductListComponent implements OnInit {
         );
     }
 
+    private products: IProduct[];
     public filteredProducts: IProduct[] = [];
 
-    private products: IProduct[] = [
-        {
-            "id": 1,
-            "name": "Leaf Rake",
-            "code": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "id": 2,
-            "name": "Garden Cart",
-            "code": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "id": 5,
-            "name": "Hammer",
-            "code": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          },
-          {
-            "id": 8,
-            "name": "Saw",
-            "code": "TBX-0022",
-            "releaseDate": "May 15, 2021",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "assets/images/saw.png"
-          },
-          {
-            "id": 10,
-            "name": "Video Game Controller",
-            "code": "GMG-0042",
-            "releaseDate": "October 15, 2020",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 4.6,
-            "imageUrl": "assets/images/xbox-controller.png"
-          }
-    ];
+    
 
     toggleImage() : void {
         this.showImage = !this.showImage;
