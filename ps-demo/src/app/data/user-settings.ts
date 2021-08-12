f86697c281559a93677a01d83f34e13c88d14005
André Davis <CubicleJockey@users.noreply.github.com>
2021-08-11 22:42:01 -0700

    About to adjust for working with a copy

---
+++ ps-demo/src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface IUserSettings {
+     name: string
+    ,emailOffers: boolean
+    ,interfaceStyles: string
+    ,subscriptionType: string
+    ,notes: string
+}
--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  //Initialize Empty
+  public userSettings: IUserSettings = {
+     name:''
+    ,emailOffers: false
+    ,interfaceStyles: ''
+    ,subscriptionType: ''
+    ,notes: ''
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    this.userSettings = {
+      name: 'André Davis'
+     ,emailOffers: true
+     ,interfaceStyles: 'dark'
+     ,subscriptionType: 'Annual'
+     ,notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis.'
+   };
   }
-
 }
